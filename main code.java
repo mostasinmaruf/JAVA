b601d8b1bbdffa1a919597595f342032eb8cf52a
Mostasin Maruf <82771221+mostasinmaruf@users.noreply.github.com>
2024-10-27 23:23:20 +0600

    Update main code.java
    Java Comments:
    Comments can explain Java code and make it more readable. They can also prevent execution when testing alternative code.
    Single-line Comments
    Single-line comments start with two forward slashes (//).
    
    Any text between // and the end of the line is ignored by Java (will not be executed).
    
    And
    
    Java Multi-line Comments
    Multi-line comments start with /* and ends with */.
    
    Any text between /* and */ will be ignored by Java.

--- main code.java
+++ main code.java
@@ -2,7 +2,9 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello Bangladesh, we are team SORDX");
      // This is a comment
-    System.out.println("I am learning Java.");
+    System.out.println("I am learning Java."); // This is a 2nd comment
     System.out.println("It is awesome!");
+    /* The code below will print the words Hello World
+    to the screen, and it is amazing */
   }
 }
