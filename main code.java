e3912def1db14ad28e8e604c7872b3757ff90990
Mostasin Maruf <82771221+mostasinmaruf@users.noreply.github.com>
2024-10-27 23:20:56 +0600

    Update main code.java

--- main code.java
+++ main code.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello Bangladesh, we are team SORDX");
+     // This is a comment
     System.out.println("I am learning Java.");
     System.out.println("It is awesome!");
   }
