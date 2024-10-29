1a43c19ca0f9ed91a8c87526a1bf2be03169b2a7
Mostasin Maruf <82771221+mostasinmaruf@users.noreply.github.com>
2024-10-29 23:13:46 +0600

    Update main code.java
    Java Data Types

--- main code.java
+++ main code.java
@@ -8,5 +8,17 @@ public class Main {
     to the screen, and it is amazing */
         String name = "SORDX";
     System.out.println(name);
+    //Java Data Types
+        int myNum = 5;               // integer (whole number)
+    float myFloatNum = 5.99f;    // floating point number
+    char myLetter = 'D';         // character
+    boolean myBool = true;       // boolean
+    String myText = "Hello";     // String    
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+  }
   }
 }
